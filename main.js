7a09561f7f7b8888ea4f04bc7fc855dfcd418d14
nlanners <lannersn@oregonstate.edu>
2021-11-04 14:11:51 -0600

    created select, update, and delete query paths

--- main.js
+++ main.js
@@ -17,10 +17,10 @@ const pool = mysql.createPool({
     database        :   'cs361_lannersn'
 });
 
-function errorCheck(err, result, res, response) {
+function errorCheck(err, res, response) {
     if(err) {
         console.log(err);
-        console.log(result);
+        response = {};
         response.status = 500;
         response.body = "Something went wrong!";
         response.error = err.code;
@@ -30,15 +30,53 @@ function errorCheck(err, result, res, response) {
     }
 }
 
-// CREATE TABLE
+function createData(req, data, columns) {
+    for (let j = 0; j < columns.length; j++) {
+        data.push(req.body.columns[columns[j]]);
+    }
+}
 
+// CREATE TABLE ?????????????????????????
 
-// SELECT ITEMS
+// SELECT ALL ITEMS
+app.get('/:table', (req, res, next) => {
+    let sql = 'SELECT * FROM ' + req.params.table;
+    let response = [];
 
+    // make query
+    pool.query(sql, (err, rows, fields) => {
+        errorCheck(err, res, response);
+        console.log(rows);
+        for (let i in rows) {
+            response.push(rows[i])
+        }
+
+        // send response
+        res.send(response);
+    })
+})
+
+// SELECT ONE ITEM
+app.get('/:table/:id', (req, res, next) => {
+    let sql = 'SELECT * FROM ' + req.params.table + ' WHERE id=?';
+    let data = [req.params.id];
+    let response = []
+
+    // make query
+    pool.query(sql, data, (err, rows, fields) => {
+        errorCheck(err, res, response);
+        for (let i in rows) {
+            response.push(rows[i])
+        }
+
+        // send response
+        res.send(response)
+    })
+})
 
 // ADD ITEMS
-app.post('/', function(req, res, next) {
-    let sql = 'INSERT INTO ' + req.body.table + ' (';
+app.post('/:table', function(req, res, next) {
+    let sql = 'INSERT INTO ' + req.params.table + ' (';
     let columns = Object.keys(req.body.columns);
     let data = [];
     let response = {};
@@ -56,30 +94,74 @@ app.post('/', function(req, res, next) {
     sql = sql.concat(');');
 
     // create data
-    for (let j = 0; j < columns.length; j++) {
-        data.push(req.body.columns[columns[j]]);
-    }
+    createData(req, data, columns)
 
     // make query
     pool.query(sql, data, function(err, result) {
-        errorCheck(err, result, res, response);
-        console.log(result);
+        errorCheck(err, res, response);
         if (result) {
             response.status = 201;
-            response.body = "INSERT was succesful!";
+            response.body = "INSERT was successful!";
             response.newID = result.insertId;
+
+            // send response
             res.send(response);
         }
     })
-
 })
 
 // UPDATE ITEMS
+app.put('/:table/:id', function(req, res, next) {
+    let sql = 'UPDATE ' + req.params.table + ' SET ';
+    let data = [];
+    let id = req.params.id;
+    let columns = Object.keys(req.body.columns);
+    let response = {};
 
+    // form query string
+    for (let i in columns) {
+        sql = sql.concat(columns[i], '=?, ');
+    }
+
+    sql = sql.slice(0, -2);
+    sql = sql.concat(' WHERE id=?;');
+
+    // create data
+    createData(req, data, columns)
+    data.push(id);
+
+    // make query
+    pool.query(sql, data, function(err, result) {
+        errorCheck(err, res, response);
+        if (result) {
+            console.log(result);
+            response.status = 201;
+            response.body = "UPDATE was successful!";
+            response.message = result.message;
+
+            // send response
+            res.send(response);
+        }
+    })
+})
 
 // DELETE ITEMS
+app.delete('/:table/:id', (req, res, next) => {
+    let sql = 'DELETE FROM ' + req.params.table + ' WHERE id=?';
+    let data = [req.params.id];
+    let response = {};
 
+    pool.query(sql, data, (err, result) => {
+        errorCheck(err, res, response);
+        if (result) {
+            response.status = 201;
+            response.body = "DELETE was successful!";
 
+            // send response
+            res.send(response);
+        }
+    })
+})
 
 // 404 ERROR
 app.use(function(req, res){
